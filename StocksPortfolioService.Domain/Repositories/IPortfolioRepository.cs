4a33dfc483677f5ad4c8a709ee2693c814adee12
Jakub Sulej <jakub.sulej@oroox.com>
2025-06-30 13:35:45 +0200

    fix

--- StocksPortfolioService.Domain/Repositories/IPortfolioRepository.cs
+++ StocksPortfolioService.Domain/Repositories/IPortfolioRepository.cs
@@ -5,5 +5,5 @@ namespace StocksPortfolioService.Domain.Repositories;
 public interface IPortfolioRepository
 {
     Task DeleteByIdAsync(string id, CancellationToken cancellationToken);
-    Task<Portfolio> GetByIdAsync(string id, CancellationToken cancellationToken);
+    Task<Portfolio?> GetByIdAsync(string id, CancellationToken cancellationToken);
 }
